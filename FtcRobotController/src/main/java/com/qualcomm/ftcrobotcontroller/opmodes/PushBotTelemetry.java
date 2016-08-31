9a5ee2f76760b03db1af0fc6a0ee080a64db06e9
SHSCyberman <seckmanroboticscyber@gmail.com>
2016-08-31 15:30:41 -0500

    Cleaned up Tabbing

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/PushBotTelemetry.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/PushBotTelemetry.java
@@ -78,27 +78,27 @@ public class PushBotTelemetry extends PushBotHardware
         // Send telemetry data to the driver station.
         //
         telemetry.addData
-            ( "01"
-            , "Left Drive: "
-                + a_left_drive_power ()
-                + ", "
-                + a_left_encoder_count ()
-            );
+                ( "01"
+                        , "Left Drive: "
+                                + a_left_drive_power ()
+                                + ", "
+                                + a_left_encoder_count ()
+                );
         telemetry.addData
-            ( "02"
-            , "Right Drive: "
-                + a_right_drive_power ()
-                + ", "
-                + a_right_encoder_count ()
-            );
+                ( "02"
+                        , "Right Drive: "
+                                + a_right_drive_power ()
+                                + ", "
+                                + a_right_encoder_count ()
+                );
         telemetry.addData
-            ( "03"
-            , "Left Arm: " + a_left_arm_power ()
-            );
+                ( "03"
+                        , "Left Arm: " + a_left_arm_power ()
+                );
         telemetry.addData
-            ( "04"
-            , "Hand Position: " + a_hand_position ()
-            );
+                ( "04"
+                        , "Hand Position: " + a_hand_position ()
+                );
 
     } // update_telemetry
 
